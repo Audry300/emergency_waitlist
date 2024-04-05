c795be378a6ca6b406651be0a81eae9b0fe34f0c
Audry300 <airad057@uottawa.ca>
2024-04-04 23:45:11 -0400

    Added functions to handle updates when removing patient from waiting list

--- emergency_waitlist/app/admin/api/AdminController.tsx
+++ emergency_waitlist/app/admin/api/AdminController.tsx
@@ -27,3 +27,51 @@ export const getPatientsInTheWaitingList = async():Promise<Patient[]> =>{
 
     return patients;
 }
+
+const updateWaitListAfterPatientRemoval = async (patient:Patient):Promise<void>=>{
+    try{
+        await prisma.patient.updateMany({
+            where:{
+                position:{gt:patient.position}
+            },
+            data:{
+                position:{decrement:1},
+                estimatedWait:{decrement:patient.estimatedWait}
+            }
+        })
+
+    } catch(error){
+        console.error('Error updating the waiting list after the patient removal',error)
+    }
+
+}
+
+export const RemovePatientFromTheWaitList = async (patient:Patient):Promise<string> => {
+    
+
+    try{
+        const {name, code} = patient
+
+        await prisma.patient.update(
+            {
+                where:{
+                    name:name,
+                    code:code
+                },
+                data:{
+                    status:'TREATED'
+                }
+            }
+        )
+
+        await updateWaitListAfterPatientRemoval(patient)
+
+        return 'success'
+
+    }catch(error){
+        console.error('Error updating the patient',error)
+        return 'error'
+
+    }
+    
+}
--- emergency_waitlist/app/admin/api/route.tsx
+++ emergency_waitlist/app/admin/api/route.tsx
@@ -1,4 +1,5 @@
-import {getPatientsInTheWaitingList} from './AdminController'
+import {getPatientsInTheWaitingList,RemovePatientFromTheWaitList} from './AdminController'
+
 
 export async function GET(){
     try{
@@ -9,7 +10,20 @@ export async function GET(){
 
     } catch(error){
         console.error('Error retrieving patients on the emergency waiting list',error);
-        return Response.json({error:'Failed retrieve patients on the emergency waiting list'})
+        return Response.json({error:'Failed to retrieve patients on the emergency waiting list'})
         
+    }
+}
+
+export async function POST (req:Request){
+    try{
+        const patientData = await req.json();
+        const response = await RemovePatientFromTheWaitList(patientData)
+        return Response.json(response);
+
+    }catch(error){
+        console.error('Error updating waiting list',error);
+        return Response.json({error:'Failed to update the waiting list'})
+
     }
 }
